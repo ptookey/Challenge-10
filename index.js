26e4f83949b312549a53a3fe9527a7346014d864
tookey95 <petetookey@gmail.com>
2023-06-25 14:35:55 -0400

    fixed error with no return in render functions

--- index.js
+++ index.js
@@ -6,7 +6,21 @@ const Square = require('./lib/square');
 
 const questions=[{type:"input", name:"letters",message:"Choose up to three Letters to use"},{type:"input", name:"textColor",message:"Chose a text color to use"},{type:"list",name:"shape",message:"Choose a shape to use",choices:["Square","Triangle","Circle"]},{type:"input", name:"shapeColor",message:"Chose a shape color to use"},]
 
-inquirer.prompt(questions).then(response=>{new Shape(response.letters, response.textColor, response.shape, response.shapeColor).render()}).then(response=>{console.log(response)}).then(response=>{createLogo('logo.svg',response)});
+inquirer.prompt(questions).then(
+   response=>{
+      if (response.shape == "Square"){
+         const svg = new Square (response.letters, response.textColor, response.shapeColor);
+         return svg.render();
+      }  else if (response.shape == "Circle"){
+         const svg = new Circle (response.letters, response.textColor, response.shapeColor);
+         return svg.render();
+      }  else if (response.shape == "Triangle"){
+         const svg = new Triangle (response.letters, response.textColor, response.shapeColor);
+         return svg.render();
+      }
+   }).then(response=>{
+      createLogo('logo.svg',response);
+   });
 
 
 function createLogo(fileName, svg){
