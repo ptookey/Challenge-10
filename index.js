99359e991c14df6bbffa2710732c840bb7f8b29c
tookey95 <petetookey@gmail.com>
2023-06-25 13:47:17 -0400

    added examples and started work on shape class

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const fs=require("fs");
 const inquirer=require("inquirer");
+const Shape = require('./lib/shapes');
 
 const questions=[{type:"input", name:"letters",message:"Choose up to three Letters to use"},{type:"input", name:"textColor",message:"Chose a text color to use"},{type:"list",name:"shape",message:"Choose a shape to use",choices:["Square","Triangle","Circle"]},{type:"input", name:"shapeColor",message:"Chose a shape color to use"},]
 
-inquirer.prompt(questions);
+inquirer.prompt(questions).then(response=>{new Shape(response.letters, response.textColor, response.shape, response.shapeColor).render()}).then(response=>{console.log(response)}).then(response=>{createLogo('logo.svg',response)});
 
+
+function createLogo(fileName, svg){
+
+    fs.writeFile(fileName,svg,(error)=>{
+        if(error){
+           console.log(error);
+        }else{
+           console.log("Svg file generated successfully!")
+        }
+      })
+}
